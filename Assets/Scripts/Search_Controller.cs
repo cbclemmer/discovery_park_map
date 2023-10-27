fc7c0e2edcef8f1b52dbebdffc600cde2fe8f89d
TheWaterMage <michaelreyesjr31@gmail.com>
2023-10-27 15:40:04 -0500

    #23 refactorizing inputupdate

--- Assets/Scripts/Search_Controller.cs
+++ Assets/Scripts/Search_Controller.cs
@@ -29,16 +29,15 @@ public class Search_Controller : MonoBehaviour, IPointerClickHandler
         search_bar.onValueChanged.AddListener(delegate{InputUpdate();});
     }
     public void InputUpdate(){
-        //Debug.Log(search_bar.text);
         class_results = main_controller.Search_Nodes(search_bar.text);
-        int class_size = class_results.Count;
 
         for(int i = 0;i < 5; i++){
-            if(i < class_size){
-                button_results[i].GetComponentInChildren<TMP_Text>().text = class_results[i].name;
+            var textLabel = button_results[i].GetComponentInChildren<TMP_Text>();
+            if(i < class_results.Count){
+                textLabel.text = class_results[i].name;
             }
             else{
-                button_results[i].GetComponentInChildren<TMP_Text>().text = "";
+                textLabel.text = "";
             }
         }
     }
