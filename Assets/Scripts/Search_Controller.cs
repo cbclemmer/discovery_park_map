578e00695a2cd7d7c42dab5bacbfaca4c8b04527
TheWaterMage <michaelreyesjr31@gmail.com>
2023-10-27 13:56:41 -0500

    Completed #23 searches nodes and displays them on buttons

--- Assets/Scripts/Search_Controller.cs
+++ Assets/Scripts/Search_Controller.cs
@@ -13,7 +13,7 @@ public class Search_Controller : MonoBehaviour, IPointerClickHandler
     public Main_Controller main_controller;
     public TMP_InputField search_bar;
     private List<Node_controller> class_results;
-    public List<GameObject> buttons_results;
+    public List<GameObject> button_results;
     public void OnPointerClick(PointerEventData eventData)
     {
         /*Debug.Log("Test");
@@ -26,18 +26,29 @@ public class Search_Controller : MonoBehaviour, IPointerClickHandler
         main_controller.state = Main_Controller.App_State.Map;
     }
     public void Start(){
-        class_results = new List<Node_controller>();
         search_bar.onValueChanged.AddListener(delegate{InputUpdate();});
     }
     public void InputUpdate(){
         //Debug.Log(search_bar.text);
         class_results = main_controller.Search_Nodes(search_bar.text);
         int class_size = class_results.Count;
-        if(class_size > 5)
+        if(class_size > 5){
             class_size = 5;
+        }
 
-        for(int i = 0;i < class_size; i++)
-            buttons_results[i].GetComponentInChildren<Text>().text = class_results[i].name;
+        for(int i = 0;i < class_size; i++){
+            if(class_results[i].name.StartsWith("Node") || class_results[i].name.StartsWith("Start")){
+                button_results[i].GetComponentInChildren<TMP_Text>().text = "";
+            }
+            else{
+                button_results[i].GetComponentInChildren<TMP_Text>().text = class_results[i].name;
+            }
+        }
+        if(class_size < 5){
+            for(int i  = class_size; i < 5; i++){
+                button_results[i].GetComponentInChildren<TMP_Text>().text = "";
+            }
+        }
     }
 
 }
