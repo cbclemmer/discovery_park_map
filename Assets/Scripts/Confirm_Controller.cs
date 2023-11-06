f03a6a9530ecf579152a3285e8ce5f2bce729b22
cbclemmer <coltonclemmerdev@gmail.com>
2023-11-06 14:53:52 -0600

    Add confirmation box

---
+++ Assets/Scripts/Confirm_Controller.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Confirm_Controller : MonoBehaviour
+{
+    public UI_Controller UIController;
+    public TMP_Text Start_Text;
+    public TMP_Text End_Text;
+
+    public void Set_Confirm_State(Node_controller endNode) {
+        if (UIController.MainController.State != Main_Controller.App_State.Search) return;
+        if (UIController.MainController.Start_Node == null) return;
+        UIController.MainController.End_Node = endNode;
+        UIController.Change_State(Main_Controller.App_State.Confirm);
+        Start_Text.text = UIController.MainController.Start_Node.Name;
+        End_Text.text = UIController.MainController.End_Node.Name;
+    }
+
+    public void Cancel()
+    {
+        if (UIController.MainController.State != Main_Controller.App_State.Confirm) return;
+        UIController.MainController.Start_Node = null;
+        UIController.MainController.End_Node = null;
+        UIController.Change_State(Main_Controller.App_State.Map);
+    }
+}
